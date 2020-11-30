7548629b93b6a934814511be91f31ab3049bc811
red0xff <gr_niboucha@esi.dz>
2020-11-30 23:37:49 +0100

    Update challenge fixing the resource location of the DLL

--- Keygen Madness/src/main.c
+++ Keygen Madness/src/main.c
@@ -1,8 +1,11 @@
 #include <windows.h>
 #include <psapi.h>
 #include <stdio.h>
+#include <time.h>
 #include <winternl.h>
-#define MYERR while(1){printf("ERROR! %d\n",GetLastError());Sleep(1000);break;}
+// uncomment to make it print errors
+//#define DEBUG
+#define MYERR while(1){ printf("ERROR! %d\n",GetLastError());Sleep(1000);break;}
 #define SIZE_EXE 1134592
 CONTEXT ctx;
 typedef __int64 QWORD;
@@ -10,8 +13,7 @@ typedef __int64 QWORD;
 typedef LONG( WINAPI * NtUnmapViewOfSection )(HANDLE ProcessHandle, PVOID BaseAddress);
 NtUnmapViewOfSection NtUnmapViewOfSection_fun;
 char charset[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-
-char blob[] = "\x01\x02\x01\x01\x01\x01\x01\x01";
+char blob[] = "\x01\x02\x01\x01\x01\x01\x01\x01\x00\x01\x01\x01\x01\x01\x01\x01\x01";
 
 VOID rand_string(LPSTR s, int size)
 {
@@ -35,14 +37,14 @@ DWORD load_res(LPCSTR res_id, PVOID* out)
 	return SizeofResource(NULL, res);
 }
 
-DWORD load_stage(PVOID* res)
+DWORD load_stage(PVOID res)
 {
-	return load_res(blob+1, res);
+	return load_res("Kerne132", res);
 }
 
-DWORD load_dll(PVOID* res)
+DWORD load_dll(PVOID res)
 {
-	return load_res(blob, res);
+	return load_res("Kernel32", res);
 }
 
 VOID hide_console()
@@ -54,6 +56,7 @@ VOID hide_console()
 
 int main(int argc, char* argv[], char* envp[])
 {
+	char dll_filename[64];
 	hide_console();
 	srand(time(NULL));
 	PEB peb;
@@ -64,26 +67,32 @@ int main(int argc, char* argv[], char* envp[])
 	memset(&processinfo, 0, sizeof(processinfo));	
 	if (!CreateProcess(/*"C:\\Windows\\System32\\calc.exe"*/argv[0], "", NULL, NULL, 0, CREATE_SUSPENDED, NULL, NULL, &startinfo, &processinfo))
 	{
-		//printf("[-] CreateProcess\n");
+#ifdef DEBUG
+		printf("[-] CreateProcess\n");
 		MYERR;
-		return 1;
+#endif
+		ExitProcess(1);
 	}
 	//HANDLE hprocess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processinfo.dwProcessId);
 	ctx.ContextFlags = CONTEXT_FULL;
 	printf("hprocess=%p\n",processinfo.hProcess);
 	if (!GetThreadContext(processinfo.hThread, &ctx))
 	{
-		//printf("[-] GetThreadContext\n");
+#ifdef DEBUG
+		printf("[-] GetThreadContext\n");
 		MYERR;
-		return 1;
+#endif
+		ExitProcess(1);
 	}
 	PVOID imagebase;
 	printf("PEB = %p\n", ctx.Rdx);
 	if (!ReadProcessMemory(processinfo.hProcess, (LPCVOID) ctx.Rdx, (LPVOID)&peb, sizeof(peb), NULL))
 	{
-		//printf("[-] ReadProcessMemory imagebase\n");
+#ifdef DEBUG
+		printf("[-] ReadProcessMemory imagebase\n");
 		MYERR;
-		return 1;
+#endif
+		ExitProcess(1);
 	}
 	imagebase = peb.Reserved3[1];
 	printf("[+] main module = %p\n", imagebase);
@@ -108,12 +117,19 @@ int main(int argc, char* argv[], char* envp[])
 	{
 		mem[i] = (mem[i] + 99) ^ 0x2f;
 	}
+	printf("Loaded stage of size %u\n", size_stage);
 	PIMAGE_DOS_HEADER dos_header = (PIMAGE_DOS_HEADER) mem;
 	PIMAGE_NT_HEADERS64 nt_header = (PIMAGE_NT_HEADERS64) ((__int64)mem + dos_header->e_lfanew);
 	printf("[?] Number of sections : %d\n", nt_header->FileHeader.NumberOfSections);
 	if (imagebase == nt_header->OptionalHeader.ImageBase)
 		if (NtUnmapViewOfSection_fun(processinfo.hProcess, (PVOID)imagebase))
+		{
+#ifdef DEBUG
 		  printf("[-] NtUnmapViewOfSection\n");
+		  MYERR;
+#endif
+		  ExitProcess(1);
+		}
 	printf("sizeof(image) = %p\n", nt_header->OptionalHeader.SizeOfImage);
 	printf("? imagebase = %p\n", imagebase);
 	LPVOID new_imagebase = VirtualAllocEx(
@@ -125,16 +141,20 @@ int main(int argc, char* argv[], char* envp[])
 	);
 	if (new_imagebase == NULL)
 	{
+#ifdef DEBUG
 		printf("[-] VirtualAllocEx imagebase\n");
 		MYERR;
-		return 1;
+#endif
+		ExitProcess(1);
 	}
 	printf("[?] new_imagebase = %p\n", new_imagebase);
 	if (!WriteProcessMemory(processinfo.hProcess, new_imagebase, mem, nt_header->OptionalHeader.SizeOfHeaders, NULL))
 	{
+#ifdef DEBUG
 		printf("[-] WriteProcessMemory headers\n");
 		MYERR;
-		return 1;
+#endif
+		ExitProcess(1);
 	}
 	for (int i = 0; i < nt_header->FileHeader.NumberOfSections; i++)
 	{
@@ -142,32 +162,40 @@ int main(int argc, char* argv[], char* envp[])
 		//printf("[?] section : %s\nVirtualAddress : 0x%x\nRaw size : %x\nPointer to raw data : %p\n", section_header->Name, section_header->VirtualAddress, section_header->SizeOfRawData, section_header->PointerToRawData);
 		if (!WriteProcessMemory(processinfo.hProcess, new_imagebase+section_header->VirtualAddress, (LPVOID)((__int64)mem+section_header->PointerToRawData), section_header->SizeOfRawData, NULL))
 		{
+#ifdef DEBUG
 			printf("[-] WriteProcessMemory section\n");
 			MYERR;
-			return 1;
+#endif
+			ExitProcess(1);
 		}
 	}
 	if (!WriteProcessMemory(processinfo.hProcess, (LPVOID)ctx.Rdx+0x10, &new_imagebase, 8, NULL))
 	{
+#ifdef DEBUG
 		printf("[-] WriteProcessMemory newimagebase\n");
 		MYERR;
-		return 1;
+#endif
+		ExitProcess(1);
 	}
 	
 	ctx.Rcx = /*nt_header->OptionalHeader.ImageBase*/(__int64)new_imagebase + nt_header->OptionalHeader.AddressOfEntryPoint;
-
-	if (!SetThreadContext(processinfo.hThread, &ctx))
+    
+    if (!SetThreadContext(processinfo.hThread, &ctx))
 	{
+#ifdef DEBUG
 		printf("[-] SetThreadContext\n");
 		MYERR;
-		return 1;
+#endif
+		ExitProcess(1);
 	}
 	
 	if (ResumeThread(processinfo.hThread) == -1)
 	{
+#ifdef DEBUG
 		printf("[-] ResumeThread\n");
 		MYERR;
-		return -1;
+#endif
+		ExitProcess(1);
 	}
 
 	// inject the dll
@@ -178,22 +206,62 @@ int main(int argc, char* argv[], char* envp[])
 	{
 		dll[i] = (dll[i] + 0x55) ^ 0xe3;
 	}
-	
-	char dll_path[] = "C:\\Users\\Redouane\\Desktop\\Work\\infosec\\megacrackme\\MzEzMzcx.dll";
-	
-	
+	//printf("[+] loaded dll of size %u\n", dll_size);
+	size_t temp_length = GetTempPathA(sizeof(dll_filename),dll_filename);
+	rand_string(dll_filename+temp_length, 5);
+	dll_filename[temp_length+5]='.';
+	dll_filename[temp_length+6]='d';
+	dll_filename[temp_length+7]='l';
+	dll_filename[temp_length+8]='l';
+	//printf("[?] dll_filename = %s\n", dll_filename);
+	OFSTRUCT fileofstruct;
+	HANDLE dll_file = CreateFile(dll_filename, GENERIC_WRITE, FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (dll_file == INVALID_HANDLE_VALUE)
+	{
+#ifdef DEBUG
+		printf("[-] CreateFile\n");
+		MYERR;
+#endif
+		ExitProcess(1);
+	}
+	//printf("dll_file = %p\n", dll_file);
+	DWORD written_bytes;
+	if (WriteFile(dll_file, dll, dll_size, &written_bytes, NULL) == 0)
+	{
+#ifdef DEBUG
+		printf("[-] WriteFile\n");
+		MYERR;
+#endif
+		ExitProcess(1);
+    }
+	CloseHandle(dll_file);
 	LPVOID dllpath_addr = VirtualAllocEx(processinfo.hProcess, NULL, _MAX_PATH, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-	WriteProcessMemory(processinfo.hProcess, dllpath_addr, dll_path, strlen(dll_path)+1, NULL);
-	
+	if (!WriteProcessMemory(processinfo.hProcess, dllpath_addr, dll_filename, strlen(dll_filename)+1, NULL))
+	{
+#ifdef DEBUG
+		printf("[-] WriteProcessMemory\n");
+		MYERR;
+#endif
+		ExitProcess(1);
+	}
 	LPVOID loadlibrary_addr = (LPVOID) GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
-	
+	if (!loadlibrary_addr)
+	{
+#ifdef DEBUG
+		printf("[-] GetProcAddress\n");
+		MYERR;
+#endif
+		ExitProcess(1);
+	}
 	HANDLE dllloader_handle = CreateRemoteThread(processinfo.hProcess, NULL, 0, loadlibrary_addr, dllpath_addr, 0, NULL);
 	
 	if (!dllloader_handle)
 	{
+#ifdef DEBUG
 		printf("[-] CreateRemoteThread\n");
 		MYERR;
-		return 1;
+#endif
+		ExitProcess(1);
 	}
 	
 	/*
@@ -204,6 +272,8 @@ int main(int argc, char* argv[], char* envp[])
 		return -1;
 	}
 	*/
-	printf("Exiting\n");
+#ifdef DEBUG
+	printf("Exiting normally\n");
+#endif
 	ExitProcess(0);
 }
--- Keygen Madness/src/rename_resource.rb
+++ Keygen Madness/src/rename_resource.rb
@@ -6,8 +6,8 @@ class String
   end
 end
 
-pattern = "\x01\x02\x01\x01\x01\x01\x01\x01"
-
+pattern = "\x02\x01\x01\x01\x01\x01\x01"
+pattern2 = 8.times.map{ 2.chr }.join
 main_exe = File.open('main.exe', 'rb', &:read)
 
 crackme_index = main_exe.index('CRACKME'.as_unicode)
@@ -22,7 +22,7 @@ if dll_index == nil
   exit 1
 end
 
-main_exe[dll_index, 8*2] = pattern.as_unicode
-main_exe[crackme_index, 7*2] = pattern[1..-1].as_unicode
+main_exe[dll_index, 8*2] = pattern2.as_unicode
+main_exe[crackme_index, 7*2] = pattern.as_unicode
 
 File.open('main.exe','wb'){|f| f.write(main_exe) }
