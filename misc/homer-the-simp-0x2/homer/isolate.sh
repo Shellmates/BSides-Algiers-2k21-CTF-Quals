9af33c6411fc41725e3451fe3ea23f9b20574fe3
hfz1337 <gh_zouahi@esi.dz>
2020-12-08 18:08:07 +0100

    hopefully the last update to this challenge

--- misc/homer-the-simp-0x2/homer/isolate.sh
+++ misc/homer-the-simp-0x2/homer/isolate.sh
@@ -3,4 +3,4 @@
 tmpdir=$(mktemp -du)
 cp -rp /home/homer $tmpdir
 head -n -2 /home/homer/.bashrc > $tmpdir/.bashrc
-/usr/bin/unshare -m --propagation unchanged bash -c "mount --bind $tmpdir /home/homer/ && su - homer"
+/usr/bin/unshare -fpm --mount-proc bash -c "mount --bind $tmpdir /home/homer/ && su - homer"
--- misc/homer-the-simp-0x2/marge/root/script.sh
+++ misc/homer-the-simp-0x2/marge/root/script.sh
@@ -3,11 +3,14 @@
 export SSHPASS="m@rge1sl1f3"
 
 sshpass -e sftp -oStrictHostKeyChecking=no -oBatchMode=no -b - sftp@homer << END
-   get -r /tmp/s3cret_d1r_fed14cdf/exam /home/marge/
+   get -r /tmp/s3cret_d1r_fed14cdf/exam /home/marge/exam
    bye
 END
 
-cd /home/marge/exam
-for archive in ./*.pth; do
-    /root/model_loader.py "$archive"
+cd /home/marge
+for archive in ./exam/*.pth; do
+    (/root/model_loader.py "$archive") &
 done
+
+sleep 10
+rm -rf ./exam
